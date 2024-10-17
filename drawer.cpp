8bf55db13a846047022999c2ec680d1e3b1e1a45
nilsmuralles <murallesnils11@gmail.com>
2024-10-17 16:55:59 -0600

    Movimiento manual de Pacman añadido

--- drawer.cpp
+++ drawer.cpp
@@ -1,16 +1,103 @@
 #include <iostream>
 #include <string>
+#include <conio.h>
 #ifdef _WIN32
 #include <windows.h>
 #endif
+using namespace std;
+
+void ShowsCursor(bool visible) {
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = visible;
+    SetConsoleCursorInfo(consoleHandle, &info);
+}
+
+void SetPosition(int16_t X, int16_t Y) {
+    HANDLE Screen = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD Position = { Y, X };
+    SetConsoleCursorPosition(Screen, Position);
+}
+
+int PrintMaze(char maze[15][55]) {
+    // Colores para los &: rojo, rosado, anaranjado, celeste
+    string colors[] = {"\033[31m", "\033[35m", "\033[32m", "\033[36m"};
+
+    // Imprimir el laberinto fila por fila con colores
+    int ampersandCount = 0;  // Contador para los diferentes colores de '&'
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 55; j++) {
+            char currentChar = maze[i][j];
+
+            if (currentChar == '#' || currentChar == '|') {
+                // Azul para las paredes (# y |)
+                cout << "\033[34m" << currentChar << "\033[0m";
+            } else if (currentChar == '&') {
+                // Colores variados para cada '&'
+                cout << colors[ampersandCount % 4] << currentChar << "\033[0m";
+                ampersandCount++;
+            } else if (currentChar == 'C') {
+                // Amarillo para el jugador (C)
+                cout << "\033[33m" << currentChar << "\033[0m";
+            } else if (currentChar == ' ') {
+                // Intercalar las 'o' entre filas: en filas pares usar 'o', en filas impares usar ' ' (vacío)
+                if (j % 2 == 1 || i == 7) {
+                    cout << "\033[37m" << ' ' << "\033[0m";  // Dejar espacio en filas impares
+                } else {
+                    cout << "\033[37m" << '.' << "\033[0m";  // Blanco para las 'o'
+                }
+            } else {
+                // Sin color para los demás caracteres
+                cout << currentChar;
+            }
+        }
+        cout << std::endl; // Nueva línea después de cada fila
+    }
+    return 0;
+}
+
+void MovePacman(char maze[15][55], int& x, int& y) {
+    char input;
+    bool running = true;
+    ShowsCursor(false);
+
+    while (running) {
+        if (_kbhit()) {
+            input = _getch();
+
+            SetPosition(x, y);
+            cout << " ";  
+
+            switch (input) {
+                case 72:
+                    if (maze[x-1][y] == ' ' || maze[x-1][y] == '.') x--;
+                    break;
+                case 80:
+                    if (maze[x+1][y] == ' ' || maze[x+1][y] == '.') x++;
+                    break;
+                case 75:
+                    if (maze[x][y-1] == ' ' || maze[x][y-1] == '.') y--;
+                    break;
+                case 77:
+                    if (maze[x][y+1] == ' ' || maze[x][y+1] == '.') y++;
+                    break;
+            }
+
+            SetPosition(x, y);
+            cout << "\033[33m" << 'C' << "\033[0m";
+
+            if(input == 'q') {
+                SetPosition(15,0);
+                running = false;
+                break;
+            }
+        }
+    }
+}
 
 int main() {
-    // Configuramos la consola para usar UTF-8 en Windows
-    #ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8);
-    #endif
-
-    // Definimos el laberinto con el símbolo especial '&' y otros caracteres
+    int x = 9, y = 24;
     char maze[15][55] = {
         "|###############################################|",
         "|        |              |              |        |",
@@ -29,45 +116,9 @@ int main() {
         "|###############################################|"
     };
 
-    // Mensaje de bienvenida
-    std::cout << "Welcome to the maze game!\n";
-    std::cout << "You are represented by the character 'C'.\n";
-    std::cout << "The exit is represented by the character '&' with different colors.\n";
-    std::cout << "You can move up, down, left, or right.\n";
-    std::cout << "Good luck!\n";
+    system("cls");
+    PrintMaze(maze);    
+    MovePacman(maze, x, y);  
 
-    // Colores para los &: rojo, rosado, anaranjado, celeste
-    std::string colors[] = {"\033[31m", "\033[35m", "\033[32m", "\033[36m"};
-
-    // Imprimir el laberinto fila por fila con colores
-    int ampersandCount = 0;  // Contador para los diferentes colores de '&'
-    for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 55; j++) {
-            char currentChar = maze[i][j];
-
-            if (currentChar == '#' || currentChar == '|') {
-                // Azul para las paredes (# y |)
-                std::cout << "\033[34m" << currentChar << "\033[0m";
-            } else if (currentChar == '&') {
-                // Colores variados para cada '&'
-                std::cout << colors[ampersandCount % 4] << currentChar << "\033[0m";
-                ampersandCount++;
-            } else if (currentChar == 'C') {
-                // Amarillo para el jugador (C)
-                std::cout << "\033[33m" << currentChar << "\033[0m";
-            } else if (currentChar == ' ') {
-                // Intercalar las 'o' entre filas: en filas pares usar 'o', en filas impares usar ' ' (vacío)
-                if (j % 2 == 1 || i == 7) {
-                    std::cout << "\033[37m" << ' ' << "\033[0m";  // Dejar espacio en filas impares
-                } else {
-                    std::cout << "\033[37m" << '.' << "\033[0m";  // Blanco para las 'o'
-                }
-            } else {
-                // Sin color para los demás caracteres
-                std::cout << currentChar;
-            }
-        }
-        std::cout << std::endl; // Nueva línea después de cada fila
-    }
     return 0;
 }
